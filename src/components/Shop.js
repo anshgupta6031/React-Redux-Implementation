b573befb5d18e6349424282477797768f7b8fce7
Ansh Gupta <guptaansh6031@gmail.com>
2024-08-02 09:40:04 +0530

    video 74 completed

---
+++ src/components/Shop.js
@@ -0,0 +1,20 @@
+
+
+
+
+import React from 'react'
+
+export default function Shop() {
+    return (
+        <div>
+            <h2 className='mb-3'>Buy Abibas shoes for 50 Rs.</h2>
+            <button className="btn btn-primary mx-2">-</button>
+            Add to cart
+            <button className="btn btn-primary mx-2">+</button>
+        </div>
+    )
+}
+
+
+
+
